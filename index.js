d4e2b90b4d25532db3a0d953c60f3208e3a1bd49
Utkarsh Dhiman <utkarshdhiman689@gmail.com>
2026-09-07 22:17:04 +0530

    Update index

--- index.js
+++ index.js
@@ -44,31 +44,31 @@ const path = require("path")
 
 
 
-const corsOptions = {
-    origin: function (origin, callback) {
-        // const allowedOrigins = ["http://localhost:5173", "http://localhost:8000", "https://heritage-ally-server.onrender.com/"];
-        const allowedOrigins = ["http://localhost:5173", "http://localhost:8000", "https://trendhive-gc44.onrender.com/"];
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         // const allowedOrigins = ["http://localhost:5173", "http://localhost:8000", "https://heritage-ally-server.onrender.com/"];
+//         const allowedOrigins = ["http://localhost:5173", "http://localhost:8000", "https://trendhive-gc44.onrender.com/"];
 
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("You Are not Authorized to Access This API"));
-        }
-    },
-    credentials: true,
-};
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error("You Are not Authorized to Access This API"));
+//         }
+//     },
+//     credentials: true,
+// };
 
 require("dotenv").config()
 require("./db-connect")
 
 const Router = require("./routes/index")
 const app = express()
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
+app.use(cors())
+
 app.use(express.json())
-app.use("/api", Router)
-
-
 app.use("/public", express.static("./public"))
+app.use("/api", Router)
 app.use(express.static(path.join(__dirname, 'dist')))
 
 
